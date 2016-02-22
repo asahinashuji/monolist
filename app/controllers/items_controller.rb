c3baf7a9a8dce338f2e4113db4d467e7a75e4140
asahinashuji <box6409@gmail.com>
2016-02-22 11:05:41 +0000

    amazon_items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,12 @@ class ItemsController < ApplicationController
 
   def new
     if params[:q]
+      require 'amazon/ecs'
+      Amazon::Ecs.options = {
+        :associate_tag => Rails.application.secrets.associate_tag,                                                                                                                                                  
+        :AWS_access_key_id => Rails.application.secrets.aws_access_key_id,                                                                                                                                  
+        :AWS_secret_key => Rails.application.secrets.aws_secret_key                                                                                                                    
+        }
       response = Amazon::Ecs.item_search(params[:q] , 
                                   :search_index => 'All' , 
                                   :response_group => 'Medium' , 
