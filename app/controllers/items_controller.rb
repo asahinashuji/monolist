bff883641024072543423de41faa7dbe92784a80
asahinashuji <box6409@gmail.com>
2016-03-01 12:51:51 +0000

    have_and_want_btn

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,6 +15,7 @@ class ItemsController < ApplicationController
                                   :response_group => 'Medium' , 
                                   :country => 'jp')
       @amazon_items = response.items
+
     end
   end
 
